4a89f93bd5775483295e7380db2a2b6ecf7ed368
VasukaLabs <vasukaman@gmail.com>
2025-02-13 08:23:03 -0500

    New char controller ad all rooms have states

--- Assets/Scripts/CoinUISystem.cs
+++ Assets/Scripts/CoinUISystem.cs
@@ -3,27 +3,27 @@ using TMPro;
 
 public class CoinUISystem : MonoBehaviour
 {
-    [SerializeField] private GameData gameData;
+    [SerializeField] private PlayerCore playerCore;
     [SerializeField] private TMP_Text coinText;
 
     void Start()
     {
-        if (gameData?.playerCore != null)
+        if (playerCore != null)
         {
-            UpdateCoinDisplay(gameData.playerCore.Coins);
-            gameData.playerCore.onCoinsChanged += UpdateCoinDisplay;
+            UpdateCoinDisplay(playerCore.Coins);
+            playerCore.onCoinsChanged += UpdateCoinDisplay;
         }
     }
 
     void OnDestroy()
     {
-        if (gameData?.playerCore != null)
-            gameData.playerCore.onCoinsChanged -= UpdateCoinDisplay;
+        if (playerCore != null)
+            playerCore.onCoinsChanged -= UpdateCoinDisplay;
     }
 
     void UpdateCoinDisplay(int amount)
     {
-        coinText.text = $"{gameData.playerCore.Coins}";
+        coinText.text = $"{playerCore.Coins}$";
       //  Debug.Log($"Coins changed by: {amount}");
     }
 }
